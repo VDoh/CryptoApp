50d8e9b6fbf8bd7cca50b72649b7a6dcdae9b8b3
Szymon Rożek <rozek1szymon@gmail.com>
2019-12-07 23:35:25 +0100

    no message

--- currency/src/main/java/com/project/currency/controlers/CryptoController.java
+++ currency/src/main/java/com/project/currency/controlers/CryptoController.java
@@ -1,12 +1,89 @@
 package com.project.currency.controlers;
 
+import com.project.currency.models.LoginForm;
+import org.apache.http.HttpEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
 public class CryptoController {
+    private static String apiKey = "31eaa970-1cc5-4934-8004-548bc722d381";
+
+    public static String makeAPICall(String uri, List<NameValuePair> parameters)
+            throws URISyntaxException, IOException {
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return response_content;
+    }
+
     @GetMapping("/crypto")
-    public String showCrypto(){
+    public String showCrypto(@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model){
+
+        model.addAttribute("misraMessages", getCryptoList());
+        String uri = "https://pro-api.coinmarketcap.com/v1/tools/price-conversion";
+        List<NameValuePair> paratmers = new ArrayList<NameValuePair>();
+        paratmers.add(new BasicNameValuePair("id","1"));
+        paratmers.add(new BasicNameValuePair("amount","1"));
+        paratmers.add(new BasicNameValuePair("convert","USD"));
+
+        try {
+            String result = makeAPICall(uri, paratmers);
+            System.out.println(result);
+        } catch (IOException e) {
+            System.out.println("Error: cannont access content - " + e.toString());
+        } catch (URISyntaxException e) {
+            System.out.println("Error: Invalid URL " + e.toString());
+        }
+
         return "crypto";
+    }
+
+    @ModelAttribute("cryptoList")
+    public List<String> getCryptoList() {
+        List<String> cryptoList = new ArrayList<String>();
+        cryptoList.add("BTC");
+        cryptoList.add("ETH");
+        cryptoList.add("BCH");
+        cryptoList.add("LTC");
+        cryptoList.add("BNB");
+        return cryptoList;
     }
 }
