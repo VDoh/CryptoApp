7067be8f5b557f46268bc8046ac7e3a3a0e55620
Szymon Rożek <rozek1szymon@gmail.com>
2019-12-08 11:12:52 +0100

    Added logic related to api calls, from 2 independent pages

--- currency/src/main/java/com/project/currency/controlers/CryptoController.java
+++ currency/src/main/java/com/project/currency/controlers/CryptoController.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import org.json.*;
 
 import org.apache.http.HttpHeaders;
 import org.apache.http.NameValuePair;
@@ -25,9 +25,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class CryptoController {
-    private static String apiKey = "31eaa970-1cc5-4934-8004-548bc722d381";
+    private static String apiKeyCoinMarket = "31eaa970-1cc5-4934-8004-548bc722d381";
+    private static String apiKeyNomics = "ba1909c740d22cc80430f8da9b9a8d19";
 
-    public static String makeAPICall(String uri, List<NameValuePair> parameters)
+    public static String makeAPICallCoinMarket(String uri, List<NameValuePair> parameters)
             throws URISyntaxException, IOException {
         String response_content = "";
 
@@ -38,7 +39,7 @@ public class CryptoController {
         HttpGet request = new HttpGet(query.build());
 
         request.setHeader(HttpHeaders.ACCEPT, "application/json");
-        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+        request.addHeader("X-CMC_PRO_API_KEY", apiKeyCoinMarket);
 
         CloseableHttpResponse response = client.execute(request);
 
@@ -51,7 +52,37 @@ public class CryptoController {
             response.close();
         }
 
-        return response_content;
+        JSONObject obj = (JSONObject) new JSONObject(response_content).get("data");
+        JSONObject objTemp= (JSONObject) obj.get("quote");
+        JSONObject objTemp2= (JSONObject) objTemp.get("USD");
+        Double priceUSD = (Double) objTemp2.get("price");
+
+        String priceStirng= priceUSD.toString();
+
+        return priceStirng;
+    }
+    public static String makeAPICallNomics()
+            throws URISyntaxException, IOException {
+            String response_content = "";
+            String url = "https://api.nomics.com/v1/currencies/ticker?key=ba1909c740d22cc80430f8da9b9a8d19&ids=BTC&interval=1d,30d&convert=USD";
+            URIBuilder query = new URIBuilder(url);
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpGet request = new HttpGet(query.build());
+            CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        JSONObject obj = (JSONObject) new JSONArray(response_content).get(0);
+        String priceUSD= (String) obj.getString("price");
+
+        return priceUSD;
     }
 
     @GetMapping("/crypto")
@@ -65,8 +96,10 @@ public class CryptoController {
         paratmers.add(new BasicNameValuePair("convert","USD"));
 
         try {
-            String result = makeAPICall(uri, paratmers);
+            String result = makeAPICallCoinMarket(uri, paratmers);
+            String result2 = makeAPICallNomics();
             System.out.println(result);
+            System.out.println( result2);
         } catch (IOException e) {
             System.out.println("Error: cannont access content - " + e.toString());
         } catch (URISyntaxException e) {
